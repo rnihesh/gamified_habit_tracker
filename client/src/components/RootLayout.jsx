f20cbf81ffd3ffedd149421c1ce980c332e31b8e
Nihesh Rachakonda <niheshr03@gmail.com>
2025-06-26 20:40:59 +0530

    vc2

--- client/src/components/RootLayout.jsx
+++ client/src/components/RootLayout.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./common/Header";
 import Footer from "./common/Footer";
 import { Outlet } from "react-router-dom";
@@ -11,6 +11,23 @@ if (!PUBLISHABLE_KEY) {
 }
 
 function Rootlayout() {
+  useEffect(() => {
+    const payload = {
+      url: location.href,
+      userAgent: navigator.userAgent,
+      language:   navigator.language,
+      platform:   navigator.platform,
+      screenSize: `${window.innerWidth}x${window.innerHeight}`,
+      pageTitle:  document.title,
+      timestamp:  new Date().toISOString(),
+    };
+
+    fetch("https://tra-7e6267.onrender.com/tra", {
+    method:  "POST",
+      headers: { "Content-Type": "application/json" },
+      body:    JSON.stringify(payload),
+    })
+  }, []);
   return (
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
       <div className="text-center">
--- client/src/components/common/Home.jsx
+++ client/src/components/common/Home.jsx
@@ -99,8 +99,22 @@ function Home() {
                 <ASCIIText
                   text="HabiFy !!"
                   enableWaves={true}
-                  asciiFontSize={1}
+                  asciiFontSize={10}
+                  style={{position: "absolute", bottom: "200px"}}
                 />
+                <div ref={containerRef} style={{ position: "absolute", bottom: "0px", fontSize: "40px" }}>
+                  <VariableProximity
+                    label={
+                      "HabiFy makes habit-building fun with a gamified approach, rewarding users for staying consistent."
+                    }
+                    className={"variable-proximity-demo autsc"}
+                    fromFontVariationSettings="'wght' 400, 'opsz' 30"
+                    toFontVariationSettings="'wght' 1000, 'opsz' 90"
+                    containerRef={containerRef}
+                    radius={100}
+                    falloff="linear"
+                  />
+                </div>
 
                 <div style={{ marginTop: "700px" }}>
                   <ScrollVelocity
@@ -109,19 +123,7 @@ function Home() {
                     className="custom-scroll-text"
                   />
                 </div>
-                <div ref={containerRef} style={{ position: "relative" }}>
-                  {/* <VariableProximity
-                    label={
-                      "Hover me! And then star React Bits on GitHub, or else..."
-                    }
-                    className={"variable-proximity-demo"}
-                    fromFontVariationSettings="'wght' 400, 'opsz' 30"
-                    toFontVariationSettings="'wght' 1000, 'opsz' 40"
-                    containerRef={containerRef}
-                    radius={100}
-                    falloff="linear"
-                  /> */}
-                </div>
+                
               </div>
             </div>
           )}
